02d0ff2c4275584f712b43c8fefd6b808e72d2a5
KatherinCruz6 <Cruzpozo0211@gmail.com>
2025-11-04 01:13:05 -0500

    img5

--- src/routes/prod.routes.js
+++ src/routes/prod.routes.js
@@ -12,10 +12,10 @@ router.get('/productos', verifyToken, getProd)
 router.get('/productos/:id', verifyToken, getProdxID)
 //router.post('/productos', verifyToken, postProd)
 //router.post('/productos', verifyToken, upload.single('imagen'), postProd)
-router.post('/productos', verifyToken, upload.single('imagen'), postProd)
+router.post('/productos', verifyToken, upload.single('prod_imagen'), postProd)
 //router.put('/productos/:id', verifyToken, putProd)
 //router.put('/productos/:id', verifyToken, upload.single('imagen'), putProd)
-router.put('/productos/:id', verifyToken, upload.single('imagen'), putProd)
+router.put('/productos/:id', verifyToken, upload.single('prod_imagen'), putProd)
 router.delete('/productos/:id', verifyToken, deleteProd)
 
 export default router
