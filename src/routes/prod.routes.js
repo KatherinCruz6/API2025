afce48a41396c558ed3019338f0e81637c73da1c
KatherinCruz6 <Cruzpozo0211@gmail.com>
2025-11-04 02:05:16 -0500

    img8

--- src/routes/prod.routes.js
+++ src/routes/prod.routes.js
@@ -12,27 +12,13 @@ router.get('/productos', verifyToken, getProd)
 router.get('/productos/:id', verifyToken, getProdxID)
 //router.post('/productos', verifyToken, postProd)
 //router.post('/productos', verifyToken, upload.single('imagen'), postProd)
-    //router.post('/productos', verifyToken, upload.single('prod_imagen'), postProd)
+router.post('/productos', verifyToken, upload.single('prod_imagen'), postProd)
 //router.put('/productos/:id', verifyToken, putProd)
 //router.put('/productos/:id', verifyToken, upload.single('imagen'), putProd)
-  //router.put('/productos/:id', verifyToken, upload.single('prod_imagen'), putProd)
+router.put('/productos/:id', verifyToken, upload.single('prod_imagen'), putProd)
 router.delete('/productos/:id', verifyToken, deleteProd)
 
 
-router.post('/productos', verifyToken, (req,res,next) => upload.single('imagen')(req,res, (err) => {
-  if (err) {
-    console.error('Error multer (post):', err);
-    return res.status(500).json({ estado:0, mensaje:'Error al procesar la imagen', error: err.message });
-  }
-  next();
-}), postProd);
 
-router.put('/productos/:id', verifyToken, (req,res,next) => upload.single('imagen')(req,res, (err) => {
-  if (err) {
-    console.error('Error multer (put):', err);
-    return res.status(500).json({ estado:0, mensaje:'Error al procesar la imagen', error: err.message });
-  }
-  next();
-}), putProd);
 
 export default router
